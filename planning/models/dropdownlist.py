3593a1b37a1602798872a7bb5a76f889eae74451
Richinel11 <mekemrichinel@gmail.com>
2026-04-21 12:56:17 +0100

    modelPlan

--- planning/models/dropdownlist.py
+++ planning/models/dropdownlist.py
@@ -3,23 +3,23 @@ import uuid
 
 class RequestingUnit(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.TextField(max_length=255)
 
 class TaskType(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.TextField(max_length=255)
 
 class ThermalPower(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.TextField(max_length=255)
 
 class NetworkTypes(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     name = models.CharField(max_length=255) 
 
-class Sections(models.Model):
+class Troncons(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    name = models.CharField(max_length=255) 
+    name = models.TextField(max_length=255) 
 
 class Consignmentcharges(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
@@ -33,7 +33,7 @@ class Segments(models.Model):
 
 class Ouvrages(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.TextField(max_length=255)
 
 class Depart(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
@@ -41,7 +41,7 @@ class Depart(models.Model):
 
 class Poste(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.TextField(max_length=255)
 
 
 
--- planning/models/travail.py
+++ planning/models/travail.py
@@ -1,5 +1,6 @@
 from django.db import models
-from dropdownlist import TaskType , Reference ,RequestingUnit,ThermalPower,NetworkTypes,Sections, TaskType , Consignmentcharges ,Segments , Ouvrages , Poste , Depart
+from dropdownlist import TaskType , Reference ,RequestingUnit,ThermalPower,NetworkTypes,Troncons, TaskType , Consignmentcharges ,Segments , Ouvrages , Poste , Depart
+from user import Utilisateur
 import uuid
 
 from dropdownlist import TaskType
@@ -13,13 +14,33 @@ class Travail(models.Model):
     depart = models.ForeignKey(Depart, on_delete=models.CASCADE) #depart
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE) #type de travail
     requesting_unit = models.ForeignKey(RequestingUnit, on_delete=models.CASCADE) #unite demanderesse
-    thermal_power = models.ForeignKey(ThermalPower, on_delete=models.CASCADE) #centrale thermique solicite
+    localite = models.CharField(max_length=255) 
     network_types = models.ForeignKey(NetworkTypes, on_delete=models.CASCADE) #type de reseau
-    sections = models.ForeignKey(Sections, on_delete=models.CASCADE) # troncons
-    Consignment_charges = models.ForeignKey(Consignmentcharges, on_delete=models.CASCADE) # consigne de charge
+    troncons= models.ForeignKey(Troncons, on_delete=models.CASCADE) # localitee impacteeelete=models.CASCADE) # consigne de charge
     Mechanical_availability = models.CharField(max_length=255) #disponibilite mecanique
     Consistency_work = models.CharField(max_length=255) #consistance du travail
     Resources_deployed = models.CharField(max_length=255) # moyens mis en oeuvre
+    observations = models.TextField(max_length=500)
+
+    # programmation temporelle
+    heure_debut_planifier = models.TimeField() # heure de debut planifie
+    Duree = models.DurationField() # heure de debut planifie
+    heure_fin_planifier = models.TimeField() # date de fin planifie  
+    date_programmer= models.DateField() # date de programmation
+    nbre_jour_avant_travaux = models.DurationField() 
+
+    #audits
+    date_creation = models.DateTimeField(auto_now_add=True) # date de creation
+    date_modification = models.DateTimeField(auto_now=True) # date de modification
+    Id_user_createur = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True, related_name="travaux_crees") # utilisateur createur
+    Id_user_modificateur = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True, related_name="travaux_modifies") # utilisateur modificateur
+
+    #indicateur d'impact
+    thermal_power = models.ForeignKey(ThermalPower, on_delete=models.CASCADE) #centrale thermique solicite
+    Puissance_sollicite = models.IntegerField()
+    puissance_interrompu = models.IntegerField()
+    previsio_ENF = models.IntegerField() #prevision ENF
+    Qte_flux_sollicite = models.IntegerField()
      
 
 
