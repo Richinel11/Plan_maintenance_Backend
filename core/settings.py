a0a14b2e92d1ce22ff5cef6b16db28f8169e92d0
MasterM-code <marcomb741@gmail.com>
2026-06-04 16:43:58 +0100

    fix mysql docker config

--- core/settings.py
+++ core/settings.py
@@ -88,35 +88,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DJANGO_DATABASE_ENGINE', 'django.db.backends.mysql'),
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DJANGO_DATABASE_NAME', 'mydb'),
-        'USER': os.environ.get('DJANGO_DATABASE_USER', 'root'),
+        'USER': os.environ.get('DJANGO_DATABASE_USER', 'appuser'),
         'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD', 'P@ssw0rd'),
-        'HOST': os.environ.get('DJANGO_DATABASE_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DJANGO_DATABASE_PORT', '3307'),
+        'HOST': os.environ.get('DJANGO_DATABASE_HOST', 'db'),
+        'PORT': os.environ.get('DJANGO_DATABASE_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
         }
     }
 }
-
-# config de la database en local
-
-# DATABASES={
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mydb',
-#         'USER': 'marco',
-#         'PASSWORD': 'P@ssw0rd',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3307',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#             'charset': 'utf8mb4',
-#         }
-#     }
-# } 
 
 # LDAP
 LDAP_SERVER = os.environ.get('LDAP_SERVER', '10.250.90.8')
